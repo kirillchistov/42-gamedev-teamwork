353cfaf5b2577ef073ddef1fdae0aa6be7c282a4
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-03-21 21:37:28 +0300

    add gravity styles, theme provider to main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from '@gravity-ui/uikit'
 import { store } from './store'
-
 import { routes } from './routes'
 import './shared/styles/normalize.pcss'
 import './shared/styles/base.pcss'
 import './shared/styles/landing.pcss'
+import '@gravity-ui/uikit/styles/fonts.css'
+import '@gravity-ui/uikit/styles/styles.css'
 
 const router = createBrowserRouter(routes)
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <ThemeProvider theme="light">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </Provider>
 )
