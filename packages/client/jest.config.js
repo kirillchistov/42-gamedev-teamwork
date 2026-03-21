05807883af8121a303be9ad79f8034ae75ee989d
Admin <kirillchistov@MacBook-Pro-Admin-2.local>
2026-03-21 13:43:30 +0300

    fix jest.config.js

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,7 +1,20 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv')
 dotenv.config()
 
-export default {
+// export default {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+//   globals: {
+//     __SERVER_PORT__: process.env.SERVER_PORT,
+//   },
+//   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+// }
+
+// и вместо export default ...
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
@@ -9,4 +22,4 @@ export default {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
-}
+};
