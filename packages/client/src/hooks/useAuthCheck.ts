f8d281c3f8d23e3253af02a64abeff8a68bd5294
TelRoY <telroychik@gmail.com>
2026-04-09 20:40:44 +0300

    Streamline auth state initialization

---
+++ packages/client/src/hooks/useAuthCheck.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux'
+import {
+  fetchUserThunk,
+  selectUserIsAuthChecked,
+} from '../slices/userSlice'
+import { AppDispatch } from '../store'
+
+export const useAuthCheck = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const isAuthChecked = useSelector(
+    selectUserIsAuthChecked
+  )
+
+  useEffect(() => {
+    if (!isAuthChecked) {
+      dispatch(fetchUserThunk())
+    }
+  }, [dispatch, isAuthChecked])
+
+  return isAuthChecked
+}
